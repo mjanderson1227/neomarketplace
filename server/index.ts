af9542c351961a5f5ca958be821b8a94e92e4ba4
Matthew <92887765+mjanderson1227@users.noreply.github.com>
2024-04-19 10:40:13 -0500

    Added user creation logic

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,8 @@
 import { loginFormSchema, registerFormSchema } from "@/schema/form-schema";
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
+import { db } from "@/server/drizzle";
+import { UserInformation } from "@/schema/drizzle/schema";
+import * as bcrypt from "bcrypt";
 
 const t = initTRPC.create();
 
@@ -8,7 +11,23 @@ export const appRouter = t.router({
     return "query success";
   }),
   userRegister: t.procedure.input(registerFormSchema).mutation(async (opts) => {
-    return "query success";
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(opts.input.password, salt);
+
+    const { confirmPassword: confirm, ...userdata } = opts.input;
+
+    if (confirm != userdata.password) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Confirm password does not match password.",
+      });
+    }
+
+    db.insert(UserInformation).values({
+      ...userdata,
+      salt: salt,
+      password: hashed,
+    });
   }),
 });
 
